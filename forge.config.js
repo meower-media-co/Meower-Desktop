ff641de725d8ded2fe52bd0cc9eaf92130ee7715
William Horning <wgyt735yt@gmail.com>
2022-02-02 19:43:54 -0500

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -24,7 +24,7 @@ module.exports = {
       config: {
         repository: {
           owner: 'meower-media-co',
-          name: 'meower-desktop',
+          name: 'meower-launcher',
         },
       },
     },
