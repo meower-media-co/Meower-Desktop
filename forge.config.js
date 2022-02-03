7ecfb3a8b70dc055551befc9a1c180268ecdb3cd
William Horning <w.horning27@gmail.com>
2022-02-02 19:41:15 -0500

    init of better desktop

---
+++ forge.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  packagerConfig: {
+    icon: './app/Images/Meower',
+    name: 'meower',
+    executableName: 'meower',
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-dmg',
+    },
+    {
+      name: 'electron-forge-maker-appimage',
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+    },
+    {
+      name: '@electron-forge/maker-zip',
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'meower-media-co',
+          name: 'meower-desktop',
+        },
+      },
+    },
+  ],
+};
